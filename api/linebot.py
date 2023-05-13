7ebee56b600133672a85d0458cef3811612a133a
Michael Chen <michael1214c@gmail.com>
2023-05-13 17:40:53 +0800

    upload file

--- api/linebot.py
+++ api/linebot.py
@@ -32,6 +32,26 @@ def callback():
         abort(400)
     return 'OK'
 
+def newest_movie():
+    movie_info = []
+    response = requests.get('https://movies.yahoo.com.tw/movie_thisweek.html')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    data = soup.find_all('div', class_ = 'release_info_text')
+
+    for index, info in enumerate(data[:10]):
+        movie_name = info.find('div', class_ = 'release_movie_name').find('a').text
+        movie_name = movie_name.strip()
+
+        try:
+            trailerUrl = soup.find_all('div',class_='release_btn color_btnbox')[index].find_all('a')[1]['href']
+        except:
+            trailerUrl = 'Yahoo電影沒有這部的預告片喔!'
+
+        imgSrc = soup.find_all('div',class_='release_foto')[index].find('img')['data-src']
+        temp = [movie_name, trailerUrl, imgSrc] #把一部電影的資訊整理成清單
+        movie_info.append(temp) #再把清單加到movie_info
+    return movie_info
+
 def movie_rank(url):
     soup = BeautifulSoup(requests.get(url).text)
     first = soup.find('dl', class_ = 'rank_list_box').find('h2').text
@@ -46,6 +66,37 @@ def movie_rank(url):
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    if event.message.text == '新片':
+
+        movie_info = newest_movie()
+        #從movie_info大清單裡，挑三個
+        movie_list = random.sample(movie_info, k = 3)
+
+        movie_template = TemplateSendMessage(
+            alt_text = 'movie template',
+            template = ImageCarouselTemplate(
+                columns = [
+                    ImageCarouselColumn(
+                        image_url = movie_list[0][2],
+                        action = URIAction(
+                            label = movie_list[0][0],
+                            uri = movie_list[0][1])),
+
+                    ImageCarouselColumn(
+                        image_url = movie_list[1][2],
+                        action = URIAction(
+                            label = movie_list[1][0],
+                            uri = movie_list[1][1])),
+
+                    ImageCarouselColumn(
+                        image_url = movie_list[2][2],
+                        action = URIAction(
+                            label = movie_list[2][0],
+                            uri = movie_list[2][1]))
+                ])
+            )
+        line_bot_api.reply_message(event.reply_token, movie_template)
 
     if event.message.text == '排行':
         button_template = TemplateSendMessage(
