586f0451aaad60d68e4c8a13ddd5f89e29287a73
Michael Chen <71140083+chfuu6@users.noreply.github.com>
2025-04-16 15:35:35 +0800

    Update linebot.py

--- api/linebot.py
+++ api/linebot.py
@@ -32,9 +32,14 @@ def callback():
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.message.text == 'test':
+        reply_message = "第36行的reply_message=改成自己想傳送的訊息"
+    else:
+        reply_message = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=reply_message))
+        
 
 if __name__ == "__main__":
     app.run()
